ed9cf5517389473a7b38415a9418b6f908c4ddc7
Uendel Nascimento <carinha23@gmail.com>
2025-07-28 22:15:41 -0300

    cabeçalo atualizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'home', component: Home},
+];
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { ViewEncapsulation } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatToolbarModule, MatMenuModule],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
+  styleUrl: './home.scss',
+
 })
 export class Home {
 
